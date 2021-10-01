3a883acf10fbcd1958c846746e20a48962845189
Jose Columbie <josejaviercolumbie@yahoo.com>
2021-09-30 23:59:06 -0700

    Add project files.

---
+++ MAUIBlazorApp.WebServer/Data/CardViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MAUIBlazorApp.WebServer.Data
+{
+    public class CardViewModel
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Img { get; set; }
+    }
+}
